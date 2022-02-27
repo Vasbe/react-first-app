66a9587ca1f4d9ccff244bb85d4dd9223cc03c14
Vasbe <vasbeme@gmail.com>
2022-02-27 13:07:14 +0400

    Added components folder

---
+++ src/Components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <header className='header'>
+            <img src='https://cdn1.ozone.ru/s3/marketing-api/banners/gl/bj/c200/glbj2i73cjf0BQEKSkOZyD9AvCf63qQA.png' />
+        </header>
+    );
+
+}
+export default Header;
---
+++ src/Components/Profile.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const Profile = () =>{
+   return( <div className='content'>
+        <div>
+          <img src ="https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/eb24e3123751965.60f559c6cd745.jpg" />
+        </div>
+        <div>
+          ava + description
+        </div>
+        <div>
+          My posts
+          <div>
+            New post
+          </div>
+          <div>
+            Post 1
+          </div>
+          <div>
+            Post 2
+          </div>
+        </div>
+      </div>);
+
+}
+
+export default Profile;
